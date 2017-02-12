caca629caea4af2eac84cbc176f6053376bd683a
Diego Castro <digoc93@hotmail.com>
2017-02-12 04:48:10 -0500

    Textos de servicios, alert modal, mejora en los formularios

--- dicoapp/static/js/dico.js
+++ dicoapp/static/js/dico.js
@@ -116,7 +116,7 @@ $(document).ready(function(){
             console.log(data);
         }, 'json' );
     });
-
+/*
     $('#contactModal button').click(function(){
         var url = URL_API + 'special_service_contact/';
         var form = $('#contactModal form');
@@ -126,7 +126,7 @@ $(document).ready(function(){
             console.log(data);
         }, 'json' );
     });
-
+*/
     var contactTest = function () {
         var url = URL_API + 'special_service_contact/';
         var form = $('#contactModal form');
@@ -153,7 +153,7 @@ $(document).ready(function(){
         return o;
     };
 
-
+/*
     $('#contactModal button').click(function(){
         var url = URL_API + 'special_service_contact/';
         var form = $('#contactModal form');
@@ -163,7 +163,7 @@ $(document).ready(function(){
             console.log(data);
         }, 'json' );
     });
-
+*/
 
     $( "#serviceSubservices" ).change(function() {
         var val = $( "#serviceSubservices" ).find(":selected").attr('price');
@@ -181,7 +181,7 @@ $(document).ready(function(){
 });
 
 
-$("button").on("click", function() { 
+$("button").on("click", function() {
     var url = URL_API + 'special_service_contact/';
         var form = $('#contactModal form');
         var jsonForm = $(form).serializeObject();
@@ -215,34 +215,208 @@ function getServices(){
 
 }
 
-function getSubService(serviceId){
-    $(".loading").show();
-    $('#serviceSubservices').html('<option value="">Selecciona tu servicio</option>');
-    $( "#servicePrice" ).val("");
+function getSubService(serviceName, srcImage){
+  var ServicesDesc = "";
+  switch (serviceName)
+  {
+		case 'Aseo':
+            ServicesDesc = "<div style=\"text-align:justify;\">"
+            ServicesDesc += "<h3 style=\"text-align:center;\">Servicio de Aseo</h3>"
+		        ServicesDesc += "<p>Dinos que días y cuantas horas necesitas un Maestro Plomero DICO.</p>";
+            ServicesDesc += "Recuerda que  todos  nuestro equipo maestro está calificado y cuenta con experiencia y habilidades certificada  por nuestro equipo administrativo.</br>";
+            ServicesDesc += "<h4>SUBSERVICIOS</h4>";
+            ServicesDesc += "<table><tr style=\"border: 1px solid black; margin-left: 20px;\"><th>Por horas aseadora</th></tr></table>";
+            ServicesDesc += "<ul style=\"list-style-type: square; margin-left: 40px;\" >";
+            ServicesDesc += "<li>Reserva pagando <strong style=\"font-size: large;\">$10000</strong> y cancela el saldo directamente a tu maestro DICO</li>"
+            ServicesDesc += "<li>El servicio no incluye materiales</li>";
+            ServicesDesc += "<li>Son 3 horas de servicio  asignadas que inician  a partir de la llegada de tu maestro DICO.</li>"
+            ServicesDesc += "<li>El  tiempo  adicional  lo acuerdas directamente con tu maestro DICO.</li>"
+            ServicesDesc += "</ul><br>";
+            ServicesDesc += "Si el maestro DICO no cumple tus expectativas, te enviaremos  otro  hasta  completar el trabajo a satisfacción.<br>";
+            ServicesDesc += "<strong style=\"font-size: large;\">VR TOTAL: $ 40.000</strong></p>";
+            ServicesDesc += "</div>";
+            break;
 
-    var url = URL_API + 'services/'+serviceId;
-    $.get( url, function(data) {
-        console.log(data);
-        $("#serviceName").html(data.name);
-        $("#serviceImage").attr('src', data.image);
-        $("#serviceDesc").html(data.desc);
+		case "Plomeria":
+            ServicesDesc = "<div style=\"text-align:justify;\">"
+            ServicesDesc += "<h3 style=\"text-align:center;\">Servicio de Plomeria</h3>"
+            ServicesDesc += "<p>Dinos que días y cuantas horas necesitas un Maestro del Aseo DICO.</p>";
+            ServicesDesc += "Recuerda que  todos  nuestro equipo maestro está calificado y cuenta con experiencia y habilidades certificada  por nuestro equipo administrativo.</br>";
+            ServicesDesc += "<h4>SUBSERVICIOS</h4>";
+            ServicesDesc += "<table>";
+            ServicesDesc += "<tr style=\"border: 1px solid black; margin-left: 20px;\"><th>Reparación e instalación de Lavaplatos/Lavadora /secadora</th></tr>";
+            ServicesDesc += "<tr style=\"border: 1px solid black; margin-left: 20px;\"><th>Reparación e instalación de Grifería (duchas, cocina, lavamanos)</th></tr>";
+            ServicesDesc += "<tr style=\"border: 1px solid black; margin-left: 20px;\"><th>Reparación e instalación de aparatos sanitarios (lavamanos, sanitarios, duchas)</th></tr>";
+            ServicesDesc += "<tr style=\"border: 1px solid black; margin-left: 20px;\"><th>Reparación de fugas de tuberías</th></tr>";
+            ServicesDesc += "</table>";
+            ServicesDesc += "<ul style=\"list-style-type: square; margin-left: 40px;\" >";
+            ServicesDesc += "<li>Reserva pagando <strong style=\"font-size: large;\">$10000</strong> y cancela el saldo directamente a tu maestro DICO</li>"
+            ServicesDesc += "<li>El servicio no incluye materiales</li>";
+            ServicesDesc += "<li>Son 2 horas de servicio  asignadas que inician  a partir de la llegada de tu maestro DICO.</li>"
+            ServicesDesc += "<li>El  tiempo  adicional  lo acuerdas directamente con tu maestro DICO.</li>"
+            ServicesDesc += "</ul><br>";
+            ServicesDesc += "Si el maestro DICO no cumple tus expectativas, te enviaremos  otro  hasta  completar el trabajo a satisfacción.<br>";
+            ServicesDesc += "<strong style=\"font-size: large;\">VR TOTAL: $ 40.000</strong></p>";
+            ServicesDesc += "</div>";
+		break;
 
-    }, 'json' );
+		case "Carpinteria":
+            ServicesDesc = "<div style=\"text-align:justify;\">"
+            ServicesDesc += "<h3 style=\"text-align:center;\">Servicio de Carpinteria</h3>"
+            ServicesDesc += "<p>Dinos que días y cuantas horas necesitas un Maestro  Carpintero DICO.</p>";
+            ServicesDesc += "Recuerda que  todos  nuestro equipo maestro está calificado y cuenta con experiencia y habilidades certificada  por nuestro equipo administrativo.</br>";
+            ServicesDesc += "<h4>SUBSERVICIOS</h4>";
+            ServicesDesc += "<table>";
+            ServicesDesc += "<tr style=\"border: 1px solid black; margin-left: 20px;\"><th>Reparación e instalación de muebles (puertas)</th></tr>";
+            ServicesDesc += "</table>";
+            ServicesDesc += "<ul style=\"list-style-type: square; margin-left: 40px;\" >";
+            ServicesDesc += "<li>Reserva pagando <strong style=\"font-size: large;\">$10000</strong> y cancela el saldo directamente a tu maestro DICO</li>"
+            ServicesDesc += "<li>El servicio no incluye materiales</li>";
+            ServicesDesc += "<li>Son 2 horas de servicio  asignadas que inician  a partir de la llegada de tu maestro DICO.</li>"
+            ServicesDesc += "<li>El  tiempo  adicional  lo acuerdas directamente con tu maestro DICO.</li>"
+            ServicesDesc += "</ul><br>";
+            ServicesDesc += "Si el maestro DICO no cumple tus expectativas, te enviaremos  otro  hasta  completar el trabajo a satisfacción.<br>";
+            ServicesDesc += "<strong style=\"font-size: large;\">VR TOTAL: $ 40.000</strong></p>";
+            ServicesDesc += "</div>";
+		break;
 
+		case "Cerrajeria":
+            ServicesDesc = "<div style=\"text-align:justify;\">"
+            ServicesDesc += "<h3 style=\"text-align:center;\">Servicio de Cerrajeria</h3>"
+            ServicesDesc += "<p>Dinos que días y cuantas horas necesitas un Maestro Cerrajero DICO.</p>";
+            ServicesDesc += "Recuerda que  todos  nuestro equipo maestro está calificado y cuenta con experiencia y habilidades certificada  por nuestro equipo administrativo.</br>";
+            ServicesDesc += "<h4>SUBSERVICIOS</h4>";
+            ServicesDesc += "<table>";
+            ServicesDesc += "<tr style=\"border: 1px solid black; margin-left: 20px;\"><th>Cambio de Guardas, cerrojo, chapas.</th></tr>";
+            ServicesDesc += "</table>";
+            ServicesDesc += "<ul style=\"list-style-type: square; margin-left: 40px;\" >";
+            ServicesDesc += "<li>Reserva pagando <strong style=\"font-size: large;\">$10000</strong> y cancela el saldo directamente a tu maestro DICO</li>"
+            ServicesDesc += "<li>El servicio no incluye materiales</li>";
+            ServicesDesc += "<li>Son 2 horas de servicio  asignadas que inician  a partir de la llegada de tu maestro DICO.</li>"
+            ServicesDesc += "<li>El  tiempo  adicional  lo acuerdas directamente con tu maestro DICO.</li>"
+            ServicesDesc += "</ul><br>";
+            ServicesDesc += "Si el maestro DICO no cumple tus expectativas, te enviaremos  otro  hasta  completar el trabajo a satisfacción.<br>";
+            ServicesDesc += "<strong style=\"font-size: large;\">VR TOTAL: $ 40.000</strong></p>";
+            ServicesDesc += "</div>";
+		break;
 
-    url = URL_API + 'sub_services/?service_id='+serviceId;
-    $.get( url, function(data) {
-        console.log(data.results);
-        $.each(data.results, function (i, item) {
-            $('#serviceSubservices').append($('<option>', {
-                value: item.id,
-                text : item.name,
-                price : item.price
-            }));
-        });
+		case "Electricista":
+            ServicesDesc = "<div style=\"text-align:justify;\">"
+            ServicesDesc += "<h3 style=\"text-align:center;\">Servicio de Electricista</h3>"
+            ServicesDesc += "<p>Dinos que días y cuantas horas necesitas un Maestro Electricista DICO.</p>";
+            ServicesDesc += "Recuerda que  todos  nuestro equipo maestro está calificado y cuenta con experiencia y habilidades certificada  por nuestro equipo administrativo.</br>";
+            ServicesDesc += "<h4>SUBSERVICIOS</h4>";
+            ServicesDesc += "<table>";
+            ServicesDesc += "<tr style=\"border: 1px solid black; margin-left: 20px;\"><th>Reparación e instalación de calentadores eléctricos</th></tr>";
+            ServicesDesc += "<tr style=\"border: 1px solid black; margin-left: 20px;\"><th>Revisión y arreglo de aparatos eléctricos, tomacorrientes, interruptores, timbres</th></tr>";
+            ServicesDesc += "<tr style=\"border: 1px solid black; margin-left: 20px;\"><th>Reparación e instalación estufas eléctricas</th></tr>";
+            ServicesDesc += "<tr style=\"border: 1px solid black; margin-left: 20px;\"><th>Instalación de lámparas (elementos  iluminación)</th></tr>";
+            ServicesDesc += "</table>";
+            ServicesDesc += "<ul style=\"list-style-type: square; margin-left: 40px;\" >";
+            ServicesDesc += "<li>Reserva pagando <strong style=\"font-size: large;\">$10000</strong> y cancela el saldo directamente a tu maestro DICO</li>"
+            ServicesDesc += "<li>El servicio no incluye materiales</li>";
+            ServicesDesc += "<li>Son 2 horas de servicio  asignadas que inician  a partir de la llegada de tu maestro DICO.</li>"
+            ServicesDesc += "<li>El  tiempo  adicional  lo acuerdas directamente con tu maestro DICO.</li>"
+            ServicesDesc += "</ul><br>";
+            ServicesDesc += "Si el maestro DICO no cumple tus expectativas, te enviaremos  otro  hasta  completar el trabajo a satisfacción.<br>";
+            ServicesDesc += "<strong style=\"font-size: large;\">VR TOTAL: $ 40.000</strong></p>";
+            ServicesDesc += "</div>";
+		break;
 
-        $(".loading").hide();
-    }, 'json' );
+		case "Gas":
+            ServicesDesc = "<div style=\"text-align:justify;\">"
+            ServicesDesc += "<h3 style=\"text-align:center;\">Servicio de Gas</h3>"
+            ServicesDesc += "<p>Dinos que días y cuantas horas necesitas un Maestro Gasodomestico DICO.</p>";
+            ServicesDesc += "Recuerda que  todos  nuestro equipo maestro está calificado y cuenta con experiencia y habilidades certificada  por nuestro equipo administrativo.</br>";
+            ServicesDesc += "<h4>SUBSERVICIOS</h4>";
+            ServicesDesc += "<table>";
+            ServicesDesc += "<tr style=\"border: 1px solid black; margin-left: 20px;\"><th>Arreglo e instalación de calentadores</th></tr>";
+            ServicesDesc += "<tr style=\"border: 1px solid black; margin-left: 20px;\"><th>Arreglo e instalación de estufas</th></tr>";
+            ServicesDesc += "<tr style=\"border: 1px solid black; margin-left: 20px;\"><th>Reparación e instalaciones de  tuberías  gas</th></tr>";
+            ServicesDesc += "</table>";
+            ServicesDesc += "<ul style=\"list-style-type: square; margin-left: 40px;\" >";
+            ServicesDesc += "<li>Reserva pagando <strong style=\"font-size: large;\">$10000</strong> y cancela el saldo directamente a tu maestro DICO</li>"
+            ServicesDesc += "<li>El servicio no incluye materiales</li>";
+            ServicesDesc += "<li>Son 2 horas de servicio  asignadas que inician  a partir de la llegada de tu maestro DICO.</li>"
+            ServicesDesc += "<li>El  tiempo  adicional  lo acuerdas directamente con tu maestro DICO.</li>"
+            ServicesDesc += "</ul><br>";
+            ServicesDesc += "Si el maestro DICO no cumple tus expectativas, te enviaremos  otro  hasta  completar el trabajo a satisfacción.<br>";
+            ServicesDesc += "<strong style=\"font-size: large;\">VR TOTAL: $ 40.000</strong></p>";
+            ServicesDesc += "</div>";
+		break;
+    case "Mudanza":
+            ServicesDesc = "<div style=\"text-align:justify;\">"
+            ServicesDesc += "<h3 style=\"text-align:center;\">Servicio de Mudanza</h3>"
+            ServicesDesc += "<p>Dinos que días y cuantas horas necesitas un Maestro de la Mudanza DICO.</p>";
+            ServicesDesc += "Recuerda que  todos  nuestro equipo maestro está calificado y cuenta con experiencia y habilidades certificada  por nuestro equipo administrativo.</br>";
+            ServicesDesc += "<h4>SUBSERVICIOS</h4>";
+            ServicesDesc += "<table>";
+            ServicesDesc += "<tr style=\"border: 1px solid black; margin-left: 20px;\"><th>Mudanzas</th></tr>";
+            ServicesDesc += "</table>";
+            ServicesDesc += "<ul style=\"list-style-type: square; margin-left: 40px;\" >";
+            ServicesDesc += "<li>Reserva pagando <strong style=\"font-size: large;\">$10000</strong> y cancela el saldo directamente a tu maestro DICO</li>"
+            ServicesDesc += "<li>El servicio no incluye materiales</li>";
+            ServicesDesc += "<li>Son 2 horas de servicio  asignadas que inician  a partir de la llegada de tu maestro DICO.</li>"
+            ServicesDesc += "<li>El  tiempo  adicional  lo acuerdas directamente con tu maestro DICO.</li>"
+            ServicesDesc += "</ul><br>";
+            ServicesDesc += "Si el maestro DICO no cumple tus expectativas, te enviaremos  otro  hasta  completar el trabajo a satisfacción.<br>";
+            ServicesDesc += "<strong style=\"font-size: large;\">VR TOTAL: $ 40.000</strong></p>";
+            ServicesDesc += "</div>";
+		break;
 
+		case "Pintor":
+            ServicesDesc = "<div style=\"text-align:justify;\">"
+            ServicesDesc += "<h3 style=\"text-align:center;\">Servicio de Pintor</h3>"
+            ServicesDesc += "<p>Dinos que días y cuantas horas necesitas un Maestro Pintor DICO.</p>";
+            ServicesDesc += "Recuerda que  todos  nuestro equipo maestro está calificado y cuenta con experiencia y habilidades certificada  por nuestro equipo administrativo.</br>";
+            ServicesDesc += "<h4>SUBSERVICIOS</h4>";
+            ServicesDesc += "<table>";
+            ServicesDesc += "<tr style=\"border: 1px solid black; margin-left: 20px;\"><th>Pintura</th></tr>";
+            ServicesDesc += "</table>";
+            ServicesDesc += "<ul style=\"list-style-type: square; margin-left: 40px;\" >";
+            ServicesDesc += "<li>Reserva pagando <strong style=\"font-size: large;\">$10000</strong> y cancela el saldo directamente a tu maestro DICO</li>"
+            ServicesDesc += "<li>El servicio no incluye materiales</li>";
+            ServicesDesc += "<li>Son 2 horas de servicio  asignadas que inician  a partir de la llegada de tu maestro DICO.</li>"
+            ServicesDesc += "<li>El  tiempo  adicional  lo acuerdas directamente con tu maestro DICO.</li>"
+            ServicesDesc += "</ul><br>";
+            ServicesDesc += "Si el maestro DICO no cumple tus expectativas, te enviaremos  otro  hasta  completar el trabajo a satisfacción.<br>";
+            ServicesDesc += "<strong style=\"font-size: large;\">VR TOTAL: $: 0 CONTACTENOS</strong></p>";
+            ServicesDesc += "</div>";
+		break;
+
+		case "Jardineria":
+            ServicesDesc = "<div style=\"text-align:justify;\">"
+            ServicesDesc += "<h3 style=\"text-align:center;\">Servicio de Jardineria</h3>"
+            ServicesDesc += "<p>Dinos que días y cuantas horas necesitas un Maestro Jardinero DICO.</p>";
+            ServicesDesc += "Recuerda que  todos  nuestro equipo maestro está calificado y cuenta con experiencia y habilidades certificada  por nuestro equipo administrativo.</br>";
+            ServicesDesc += "<h4>SUBSERVICIOS</h4>";
+            ServicesDesc += "<table>";
+            ServicesDesc += "<tr style=\"border: 1px solid black; margin-left: 20px;\"><th>Servicios generales de jardinería</th></tr>";
+            ServicesDesc += "</table>";
+            ServicesDesc += "<ul style=\"list-style-type: square; margin-left: 40px;\" >";
+            ServicesDesc += "<li>Reserva pagando <strong style=\"font-size: large;\">$10000</strong> y cancela el saldo directamente a tu maestro DICO</li>"
+            ServicesDesc += "<li>El servicio no incluye materiales</li>";
+            ServicesDesc += "<li>Son 2 horas de servicio  asignadas que inician  a partir de la llegada de tu maestro DICO.</li>"
+            ServicesDesc += "<li>El  tiempo  adicional  lo acuerdas directamente con tu maestro DICO.</li>"
+            ServicesDesc += "</ul><br>";
+            ServicesDesc += "Si el maestro DICO no cumple tus expectativas, te enviaremos  otro  hasta  completar el trabajo a satisfacción.<br>";
+            ServicesDesc += "<strong style=\"font-size: large;\">VR TOTAL: $: 40.000</strong></p>";
+            ServicesDesc += "</div>";
+		break;
+
+	}
+
+  $("#serviceName").html(serviceName);
+  $("#serviceImage").attr('src', srcImage);
+  $("#serviceDesc").html(ServicesDesc);
 }
 
+function sendContactenos(){
+  $("#titleAlertModal").html("Mensaje Enviado");
+  $("#textAlertModal").html("Tu mensaje ha sido enviado, nos estaremos comunicando lo antes posible.");
+}
+
+function sendTrabajaCon(){
+  $("#titleAlertModal").html("Mensaje Enviado");
+  $("#textAlertModal").html("Tu mensaje ha sido enviado, gracias por querer trabajar con DICO.<br> protno recibiras noticias.");
+}
